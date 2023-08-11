7d34be181f8f2dc64dfcd10e248b53fb609e3aef
StefanoGalandrini <stefe68@gmail.com>
2023-08-11 19:42:03 +0200

    Add more playing logic and style

--- src/components/AppGame.vue
+++ src/components/AppGame.vue
@@ -8,18 +8,20 @@ export default {
 				{
 					role: "user",
 					content:
-						'Stiamo simulando il gioco "Chi vuole essere milionario?". In questo gioco, il concorrente deve rispondere a una serie di massimo 15 domande a difficoltà crescente,partendo da un livello molto facile, per vincere un premio massimo di un milione di euro. Per ogni domanda ci saranno quattro risposte, ma una sola è corretta. Ad esempio, se la domanda fosse "Qual è la capitale della Spagna", le risposte potrebbero essere "Barcellona, Madrid, Malaga, Siviglia". Tu sei il direttore del gioco, e senza mai fare riferimento a te stesso devi generare una domanda lunga al massimo 25 parole su un argomento a tua scelta, e fornire quattro possibili risposte indicando quale è la risposta corretta. Devi restituire come risposta solo un array con tre elementi: nel primo metterai solo la domanda, nel secondo un array con le quattro risposte, nel terzo l\'indice della risposta esatta. Non devi aggiungere altre spiegazioni. Ad ogni richiesta troverai tutti gli array che hai generato in modo da aumentare gradualmente la difficoltà delle domande. Inizia con la prima domanda, molto facile.',
+						'Stiamo simulando il gioco "Chi vuole essere milionario?". In questo gioco, il concorrente deve rispondere a una serie di massimo 15 domande a difficoltà crescente,partendo da un livello molto facile, per vincere un premio massimo di un milione di euro. Per ogni domanda ci saranno quattro risposte, ma una sola è corretta. Ad esempio, se la domanda fosse "Qual è la capitale della Spagna", le risposte potrebbero essere "Barcellona, Madrid, Malaga, Siviglia". Tu sei il direttore del gioco, e senza mai fare riferimento a te stesso devi generare una domanda lunga al massimo 25 parole su un argomento a tua scelta, e fornire quattro possibili risposte indicando quale è la risposta corretta. Devi restituire come risposta solo un oggetto con tre elementi: nel primo metterai solo la domanda con la chiave "question"; nel secondo, con la chiave "anwers", un array con le quattro risposte precedute dalle prime quattro lettere dell\'alfabeto e i due punti con uno spazio dopo (esempio: "A: Risposta 1"); nel terzo, con la chiave "correct", l\'indice della risposta esatta. Non devi aggiungere altre spiegazioni. Ad ogni richiesta troverai tutti gli array che hai generato in modo da aumentare gradualmente la difficoltà delle domande. Inizia con la prima domanda, molto facile.',
 				},
 			],
 			currentImage: "",
 			currentQuestion: "",
 			currentAnswers: [],
+			correct: null,
 			level: 1,
-			selectedAnswer: null,
+			selectedAnswerIndex: null,
 			baseUrl: "http://localhost:8000",
 			chatEndpoint: "/api/chat",
 			imageEndpoint: "/api/image",
 			isLoading: false,
+			isAnswerSelected: false,
 		};
 	},
 
@@ -36,8 +38,9 @@ export default {
 				this.chatEndpoint,
 				chatPayload,
 			);
-			this.currentQuestion = chatResponse;
-			console.log(chatResponse);
+			this.currentQuestion = chatResponse.question;
+			this.currentAnswers = chatResponse.answers;
+			this.correct = chatResponse.correct;
 
 			// Aggiunge la risposta alla chatHistory
 			this.chatHistory.push({
@@ -62,8 +65,20 @@ export default {
 			return response.data.response;
 		},
 
+		getButtonClass(index) {
+			return {
+				selected: index === this.selectedAnswerIndex,
+				discarded:
+					this.selectedAnswerIndex !== null &&
+					index !== this.selectedAnswerIndex,
+			};
+		},
+
 		selectAnswer(index) {
-			this.selectedAnswer = index;
+			if (!this.isAnswerSelected) {
+				this.selectedAnswerIndex = index;
+				this.isAnswerSelected = true;
+			}
 		},
 
 		prizeForLevel(l) {
@@ -72,6 +87,24 @@ export default {
 				500000, 750000, 900000, 950000, 1000000,
 			];
 			return prizes[l - 1];
+		},
+
+		levelClass(l) {
+			if (l === this.level) {
+				return "current-level";
+			} else if (l < this.level) {
+				return "passed-level";
+			}
+			return "";
+		},
+
+		getResult() {
+			if (this.selectedAnswerIndex === this.correct) {
+				this.level++;
+				return "CORRETTA!";
+			} else {
+				return "sbagliata...";
+			}
 		},
 	},
 
@@ -89,28 +122,38 @@ export default {
 		</div>
 
 		<!-- Game Grid -->
-		<div v-if="!isLoading" class="image-section">
-			<img :src="currentImage" alt="" />
-		</div>
-		<div v-if="!isLoading" class="question-section">
-			<p>{{ currentQuestion }}</p>
-			<div class="answers">
-				<button
-					v-for="(answer, index) in currentAnswers"
-					:key="index"
-					:class="{selected: selectedAnswer === index}"
-					@click="selectAnswer(index)">
-					{{ answer }}
-				</button>
+		<div v-if="!isLoading">
+			<div class="image-section">
+				<img :src="currentImage" alt="" />
+			</div>
+
+			<div class="question-section">
+				<p class="question">{{ currentQuestion }}</p>
+				<div class="answers">
+					<button
+						class="answer"
+						v-for="(answer, index) in currentAnswers"
+						:key="index"
+						:class="getButtonClass(index)"
+						:disabled="isAnswerSelected"
+						@click="selectAnswer(index)">
+						{{ answer }}
+					</button>
+				</div>
 			</div>
 		</div>
 
 		<!-- Prize Grid -->
 		<div class="level-prize-section">
-			<div v-for="l in 15" :key="l" :class="{'current-level': l === level}">
+			<div v-for="l in 15" :key="l" :class="levelClass(l)">
 				<span>Livello {{ l }}</span>
 				<span>{{ prizeForLevel(l) }} €</span>
 			</div>
+		</div>
+
+		<div v-if="isAnswerSelected">
+			<p class="answered">La risposta è...</p>
+			<p class="result">{{ getResult() }}</p>
 		</div>
 	</div>
 </template>
@@ -128,6 +171,14 @@ export default {
 		display: flex;
 		justify-content: center;
 		align-items: center;
+
+		img {
+			width: 400px;
+			margin-top: 2rem;
+			border: 2px solid white;
+			border-radius: 30px;
+			overflow: hidden;
+		}
 	}
 
 	.question-section {
@@ -137,17 +188,43 @@ export default {
 		justify-content: center;
 		align-items: center;
 
+		.question {
+			margin-top: 3rem;
+			font-size: 2rem;
+		}
+
 		.answers {
+			margin-top: 2rem;
+			width: 70%;
 			display: flex;
-			flex-direction: column;
+			justify-content: center;
+			flex-wrap: wrap;
+			gap: 1.5rem;
 
 			button {
-				background-color: $color-bg;
-				&:hover {
-					background-color: $color-prize;
-				}
+				width: 40%;
+				color: lightgrey;
+				background-color: $color-button;
+				padding: 0.5rem 1.5rem;
+				font-size: 1.25rem;
+				border: 1px solid grey;
+				transition: all 250ms;
+
 				&.selected {
-					background-color: green;
+					background-color: $color-chosen;
+					color: white;
+					box-shadow: 0 0 3px 3px rgba(255, 255, 255, 0.5);
+					border: 1px solid white;
+				}
+
+				&.discarded {
+					background-color: $color-discarded;
+				}
+
+				&:hover {
+					color: white;
+					box-shadow: 0 0 3px 3px rgba(255, 255, 255, 0.5);
+					border: 1px solid white;
 				}
 			}
 		}
@@ -156,18 +233,30 @@ export default {
 	.level-prize-section {
 		grid-row: 1 / 3;
 		grid-column: 2 / 3;
-		padding: 3rem 6rem;
+		padding: 3rem 4rem;
 		display: flex;
 		flex-direction: column-reverse;
 		justify-content: space-around;
-		// align-items: space-between;
 
 		div {
 			display: flex;
 			justify-content: space-between;
+			color: lightgray;
+
+			span {
+				padding: 0.5rem 1.5rem;
+			}
 
 			&.current-level {
 				font-weight: bold;
+				color: white;
+				background-color: $color-discarded;
+				border-radius: 10px;
+			}
+
+			&.passed-level {
+				color: $color-discarded;
+				background-color: transparent;
 			}
 		}
 	}
